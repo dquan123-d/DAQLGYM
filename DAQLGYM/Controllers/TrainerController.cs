b1a8687cda979d8a3dc84f43e8ef9372eec6a628
dquan1809 <dquan0679@gmail.com>
2025-01-09 14:59:59 +0700

    create trainer

---
+++ DAQLGYM/Controllers/TrainerController.cs
@@ -0,0 +1,20 @@
+﻿using DAQLGYM.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAQLGYM.Controllers
+{
+    public class TrainerController : Controller
+    {
+        private readonly QlgymContext _context;
+
+        public TrainerController(QlgymContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            //var trainer = _context.TblTrainer
+            return View();
+        }
+    }
+}
