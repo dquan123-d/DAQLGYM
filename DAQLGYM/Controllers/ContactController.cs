feb0ac6148a5c75a3581e569fba83e07321ab787
dquan1809 <dquan0679@gmail.com>
2025-01-11 14:57:58 +0700

    hoàn thành 90%

--- DAQLGYM/Controllers/ContactController.cs
+++ DAQLGYM/Controllers/ContactController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DAQLGYM.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DAQLGYM.Controllers
 {
     public class ContactController : Controller
     {
+
+        private readonly QlgymContext _context;
+
+        public ContactController(QlgymContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TblContact contact)
+        {
+            contact.ModifiedDate = DateTime.Now;
+            contact.CreatedDate = DateTime.Now;
+            contact.CreatedBy = contact.Name;
+            contact.ModifiedBy = contact.Name;
+            _context.TblContacts.Add(contact);
+            _context.SaveChanges();
+            return View(contact);
         }
     }
 }
